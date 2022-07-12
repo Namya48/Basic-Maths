329747868167957a945f5b8a69d33515ae8a80bf
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-12 08:44:18 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,14 @@
-public class sum
+import java.util.*;
+public class sum 
 {
-    public static void main(int a,int b){ 
-    System.out.print(a+b);
+    public static void main(String[]args)
+    {
+        Scanner sc =new Scanner (System.in);
+        System.out.println("Enter First Number");
+        float a = sc.nextInt();
+        System.out.println("Enter Second Number");
+        float b = sc.nextInt();
+        double c= a+b;
+        System.out.print("The Sum of Two Number is:"+c);
+    }
 }
-}  
