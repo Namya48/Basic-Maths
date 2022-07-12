e70bc3a4bccc8e0ce30dad32ccb4ca1b3b846dcd
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-12 09:12:49 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -1,8 +1,13 @@
-
-public class multiply
-
-{
-    public static void main(int a,int b){ 
-    System.out.print(a*b);
+import java.util.*;
+public class multiply {
+    public static void main(String[]args)
+    {
+        Scanner sc =new Scanner (System.in);
+        System.out.println("Enter First Number");
+        float a = sc.nextInt();
+        System.out.println("Enter Second Number");
+        float b = sc.nextInt();
+        double c= a*b;
+        System.out.print("The Multiplication of Two Number is:"+c);
+    }
 }
-}  
