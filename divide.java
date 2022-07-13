6933ef9072fff35c5c99dd0da77dfb851022baef
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-13 08:08:56 +0530

    Update divide.java

--- divide.java
+++ divide.java
@@ -1,8 +1,14 @@
-
-public class calculations
-
+import java.util.*;
+public class divide
 {
-    public static void main(int a,int b){ 
-    System.out.print(a/b);
+    public static void main(String[]args)
+    {
+        Scanner sc =new Scanner (System.in);
+        System.out.println("Enter First Number");
+        float a = sc.nextFloat();
+        System.out.println("Enter Second Number");
+        float b = sc.nextFloat();
+        double c= a/b;
+        System.out.print("The Division of Two Number is:"+c);
+    }
 }
-}  
